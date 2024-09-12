fc7195f5489e202c8d99dbccae15409aeb3e3343
Gabby Gonzalez <serenamidori@gmail.com>
2024-09-12 11:52:20 -0500

    major update to replace bootstrap with tailwind, avoiding using CDN, minor color update, removal of rows and cols to replace with flexbox and match current responsiveness.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./css/**/*.{html,js}"],
+  content: ["./_layouts/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
